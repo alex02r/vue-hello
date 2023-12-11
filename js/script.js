b6a47af99c2632a57a3c16018b5c10b82893a345
alex02r <132152331+alex02r@users.noreply.github.com>
2023-12-11 15:25:58 +0100

    [UP] Aggiunta dell'immagine tramite vue

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            text: 'Messaggio da visualizzare'
+            text: 'Minecraft',
+            img: 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_Minecraft_image1600w.jpg'
         }
     }
 }).mount('#app');
